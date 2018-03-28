5376209c9ac557d6a0a9a8773c5da4862f5cb670
Koichi.Shimomura <unieye51@gmail.com>
2018-03-28 15:20:18 +0900

    rakuten api

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328004220) do
+ActiveRecord::Schema.define(version: 20180328060345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.string "url"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
