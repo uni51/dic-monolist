3dd82eb7d7b6d565df34c7762a9edbe4624e843c
uni51 <uni51@users.noreply.github.com>
2018-03-30 14:41:39 +0900

    アイコンのバリデーション順位を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
-  validates :icon, presence: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  validates :icon, presence: true
 
   has_many :ownerships
   has_many :items, through: :ownerships
